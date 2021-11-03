21a8f2a8c30de1fcc31c4d95fd43a55fce05111d
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-03 19:26:29 -0400

    Inititial - Nov. 4 2021

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+	  domains: ['cdn.ady.systems', 'ady.systems', 'auth.ady.world'],
+	},
+  experimental: {
+    urlImports: ['https://stackpath.bootstrapcdn.com', 'https://cdn.ady.systems/', 'https://cdn.skypack.dev/', 'https://auth.ady.world'],
+  },
+};
