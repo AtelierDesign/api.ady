f0f2b4aca3c5e92ae1085687fa9d96e64f54b6a4
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-06 14:10:51 -0400

    Update profile.jsx

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,56 +1,21 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
-import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
-import Image from 'next/link';
+import Image from 'next/image';
+import { useUser } from '@auth0/nextjs-auth0';
 
-import Loading from '../components/Loading';
-import ErrorMessage from '../components/ErrorMessage';
-import Highlight from '../components/Highlight';
+export default function Profile() {
+  const { user, error, isLoading } = useUser();
 
-import PageLink from '../components/PageLink';
-
-function Profile() {
-  const { user, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   return (
-    <>
-      {isLoading && <Loading />}
-      {user && (
-        <>
-          <Row className="align-items-center profile-header mb-5 text-center text-md-left" data-testid="profile">
-            <Col md={2}>
-              <Image
-                src={user.picture}
-                width={50}
-                height={50}
-                alt={user.name}
-                className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-                decode="async"
-                data-testid="profile-picture"
-              />
-            </Col>
-            <Col md>
-              <h2 data-testid="profile-name">{user.name}</h2>
-              <p className="lead text-muted" data-testid="profile-email">
-                {user.email}
-              </p>
-              <p>Take me to
-                <PageLink href="/downloads" className="page-link" testId="my-downloads">
-                  My Downloads
-                </PageLink>
-              </p>
-            </Col>
-          </Row>
-          <Row data-testid="profile-json">
-            <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
-          </Row>
-        </>
-      )}
-    </>
+    user && (
+      <div>
+        <Image src={user.picture} alt={user.name} width={40} height={40} />
+        <h3>Here is your Profile Information.</h3>
+        <h4>{user.name}</h4>
+        <h4>{user.email}</h4>
+      </div>
+    )
   );
 }
-
-export default withPageAuthRequired(Profile, {
-  onRedirecting: () => <Loading />,
-  onError: error => <ErrorMessage>{error.message}</ErrorMessage>
-});
