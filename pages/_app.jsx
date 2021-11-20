3d90fffbfcf56010333ec5193478e1074bef6a83
chvndler <70621588+chvndler@users.noreply.github.com>
2021-11-20 15:37:12 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,12 +5,6 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 
 import Layout from '../components/Layout';
 
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import initFontAwesome from '../utils/initFontAwesome';
-import '../styles/globals.scss';
-
-
-initFontAwesome();
 
 export default function App({ Component, pageProps }) {
   return (
