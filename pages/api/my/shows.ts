3f15011c9db41ce00f2e641f0a3037932bb9a3f7
chvndler <chan@ady.world>
2022-01-11 00:37:36 -0500

    Update shows.ts

--- pages/api/my/shows.ts
+++ pages/api/my/shows.ts
@@ -4,7 +4,7 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
 const verifyJwt = NextJwtVerifier({
   issuer: process.env.AUTH0_ISSUER_BASE_URL,
-  audience: process.env.AUTH0_AUDIENCE
+  audience: process.env.AUTH0_AUDIENCE,
 });
 
 const requireScope = (scope: string, apiRoute: NextApiHandler) =>
@@ -13,7 +13,7 @@ const requireScope = (scope: string, apiRoute: NextApiHandler) =>
     if (!claims || !claims.scope || (claims.scope as string).indexOf(scope) === -1) {
       return res.status(403).json({
         error: 'access_denied',
-        error_description: `Token does not contain the required '${scope}' scope`
+        error_description: `Token does not contain the required '${scope}' scope`,
       });
     }
     return apiRoute(req, res);
@@ -22,14 +22,14 @@ const requireScope = (scope: string, apiRoute: NextApiHandler) =>
 const apiRoute = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const response = await fetch('https://api.tvmaze.com/search/shows?q=identity');
-    const shows = await response.json();
 
+    const shows = await response.json();
     res.status(200).json({ shows });
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).json({
       code: error.code,
-      error: error.message
+      error: error.message,
     });
   }
 };
